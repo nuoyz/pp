aab4e98011dc694726e47ac30b8a5f5e45693a80
nuoyz <1670145451@qq.com>
2018-07-03 20:16:32 +0800

    按钮 唱名 修改样式修改

--- src/score.jsx
+++ src/score.jsx
@@ -73,11 +73,14 @@ export default class Score extends React.Component {
         return (
           <div className="sheet">
             <div className="sheet-header">
-              <button className="SH-btn"  onClick={this.play}>{uiState}</button>
+              <button className="play btn btn--a mbm" onClick={this.play}><span class="fa fa-headphones" aria-hidden="true"></span>{uiState}</button>   
               <h5 className="SH-score">分数： {window.score}</h5>
             </div>
-            <div className="sheet-content">{solfeggio.value}</div>
+            <div className="sheet-content-text">{solfeggio.value}</div>
          </div>
         )
     }
   }
+
+  //<button className="SH-btn"  onClick={this.play}>{uiState}</button>
+  //<div className="sheet-content">{solfeggio.value}</div>
